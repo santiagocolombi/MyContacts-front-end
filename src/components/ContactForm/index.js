722f462ed94f8d80722c708ec87f644f74e7b075
santiagocolombi <santiagocolombi@hotmail.com>
2025-03-20 09:22:38 -0300

    Validando dados de email e nome na tinterface

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -3,27 +3,97 @@ import { Form, BuutonContainer } from "./styles";
 import Input from '../input'
 import Select from '../Select';
 import Button from '../Button';
-import PropTypes from "prop-types";
+import PropTypes, { func } from "prop-types";
+import { use, useState} from "react";
+import isEmailValid from "../../utils/isEmailValid";
 
 
 export default  function ContactForm({buttonLabel}){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [category, setCategory] = useState(''); //controlled component
+    const [errors, setErrors] = useState([]);
+
+
+
+
+    function hadleNameChange(event) {
+        setName(event.target.value);
+
+        if(!event.target.value){
+            setErrors((prevState) => [
+                ...prevState,
+                {field: 'name', message: 'Nome é obrigatório'},
+
+
+            ]);
+        } else {
+            setErrors((prevState) => prevState.filter(
+                (error) => error.field !== 'name',
+
+            ));
+        }
+    }
+
+    function handleEmailChange(event){
+        setEmail(event.target.value);
+
+        if(event.target.value && !isEmailValid(event.target.value)){
+            const errorAlreadyExists = errors.find((error) => error.field === 'email' );
+            if(errorAlreadyExists){
+                return;
+            }
+            setErrors((prevState) => [
+                ...prevState,
+                {field: 'email', message: 'E-mail inválido'},
+
+
+            ]);
+
+        } else{
+            setErrors((prevState) => prevState.filter(
+                (error) => error.field !== 'email',
+
+            ));
+        }
+
+    }
+    function getErrorMessageByFieldName(fieldName){
+        return errors.find((error) => (error) => error.field === fieldName)?.message;
+    }
+    function handleSubmit(event){
+        event.preventDefault();
+    }
+    console.log(getErrorMessageByFieldName('name'))
     return (
-        <Form>
-           <FormGroup>
-            <Input placeholder="Nome"/>
+        <Form onSubmit={handleSubmit}>
+           <FormGroup error = {getErrorMessageByFieldName('name')}>
+            <Input value={name} placeholder="Nome"
+            error = {getErrorMessageByFieldName('name')}
+            onChange={hadleNameChange}/>
            </FormGroup>
 
-           <FormGroup error="O formato do E-mail é inválido">
-                <Input placeholder="E-mail" error />
+           <FormGroup error = {getErrorMessageByFieldName('email')}>
+                <Input
+                error = {getErrorMessageByFieldName('email')}
+                placeholder="E-mail"
+                value={email}
+                onChange={handleEmailChange}/>
             </FormGroup>
 
            <FormGroup>
-            <Input placeholder="Telefone"/>
+            <Input placeholder="Telefone"
+            value={phone}
+            onChange={(event) => setPhone(event.target.value)}/>
            </FormGroup>
 
            <FormGroup>
-            <Select>
+            <Select value={category}
+            onChange={(event) => setCategory(event.target.value)}>
+                <option value="">Categoria</option>
                 <option value="instagram">Instagram</option>
+                <option value="discord">Discord</option>
             </Select>
            </FormGroup>
            <BuutonContainer>

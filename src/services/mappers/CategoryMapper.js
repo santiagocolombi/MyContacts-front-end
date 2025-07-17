02b595ceeaf852eb84963af9ec9e4a8da7828622
santiagocolombi <santiagocolombi@hotmail.com>
2025-07-17 11:12:45 -0300

    criando o CategorryMapper

---
+++ src/services/mappers/CategoryMapper.js
@@ -0,0 +1,11 @@
+class CategoryMapper{
+    toDomain(persistenceCategory){
+        return{
+            id: persistenceCategory.id,
+            name: persistenceCategory.name,
+        }
+    }
+
+}
+
+export default new CategoryMapper();
